0ae8cfcbab02f83d3359e777d3ca66e1b77a98fd
hanfanw <hanfanw@gmail.com>
2019-05-13 01:46:01 -0400

    add todo-item

--- index.js
+++ index.js
@@ -40,3 +40,25 @@ var app5 = new Vue({
         }
     }
 });
+
+var app6 = new Vue({
+    el: "#app-6",
+    data: {
+        message: "Hello Vue!"
+    }
+});
+
+Vue.component("todo-item", {
+    // todo-item 组件现在接受一个
+    // "prop"，类似于一个自定义特性。
+    // 这个 prop 名为 todo。
+    props: ["todo"],
+    template: "<li>{{ todo.text }}</li>"
+});
+
+var app7 = new Vue({
+    el: "#app-7",
+    data: {
+        groceryList: [{ id: 0, text: "蔬菜" }, { id: 1, text: "奶酪" }, { id: 2, text: "随便其它什么人吃的东西" }]
+    }
+});
